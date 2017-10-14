13843882daa4ef574bde7a5b04653022b29d33d4
Rasmita Dash <dashr@timeinc.com>
2017-10-15 00:39:16 +0530

    initial checkin

---
+++ public/script.js
@@ -0,0 +1,28 @@
+
+    var endtime = 'November 19 2017 18:59:59 GMT+0200';
+        function updateClock(){
+            var t = getTimeRemaining(endtime);
+            document.getElementById('timerDay').innerHTML = t.days;
+            document.getElementById('timerHour').innerHTML = t.hours;
+            document.getElementById('timerMinute').innerHTML = t.minutes;
+            document.getElementById('timerSecond').innerHTML = t.seconds;
+
+            if(t.total<=0){
+                clearInterval(timeinterval);
+            }
+        }
+
+        function getTimeRemaining(endtime){
+            var t = Date.parse(endtime) - Date.parse(new Date());
+            var seconds = Math.floor( (t/1000) % 60 );
+            var minutes = Math.floor( (t/1000/60) % 60 );
+            var hours = Math.floor( (t/(1000*60*60)) % 24 );
+            var days = Math.floor( t/(1000*60*60*24) );
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        }
